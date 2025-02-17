34bbc6f9f6241fa56223e8b43b96f503c6cc86a0
Donghwi_Park <gptnc5100@gmail.com>
2025-02-17 11:33:47 +0100

    Specifying the Data Provider and Connection String

--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -12,7 +12,11 @@ namespace EntityFrameworkCore.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
+            //Using SQL Server
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeage_EfCore; Encrypt=False");
+
+            //Using SQLite
+            optionsBuilder.UseSqlite($"Data Source=FootballLeage_EfCore.db");
         }
 
         public DbSet<Team> Teams { get; set; }
