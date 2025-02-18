42d35eda2b9d890adc669cd33f500985e77eecd9
Donghwi_Park <gptnc5100@gmail.com>
2025-02-18 10:14:13 +0100

    Seeding Data

--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -19,6 +19,35 @@ namespace EntityFrameworkCore.Data
             optionsBuilder.UseSqlite($"Data Source=FootballLeage_EfCore.db");
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                new Team
+                {
+                    TeamId = 1,
+                    Name = "Liverpool",
+                    CreatedDate = new DateTime(2023, 1, 1, 01, 01, 01, DateTimeKind.Utc)
+                },
+                 new Team
+                 {
+                     TeamId = 2,
+                     Name = "Arsenal",
+                     CreatedDate = new DateTime(2023, 1, 1, 02, 02, 02, DateTimeKind.Utc)
+                 },
+                  new Team
+                  {
+                      TeamId = 3,
+                      Name = "Tottenham Hotspur",
+                      CreatedDate = new DateTime(2023, 1, 1, 03, 03, 03, DateTimeKind.Utc)
+                  },
+                   new Team
+                   {
+                       TeamId = 4,
+                       Name = "Manchester City",
+                       CreatedDate = new DateTime(2023, 1, 1, 04, 04, 04, DateTimeKind.Utc)
+                   });
+        }
+
         public DbSet<Team> Teams { get; set; }
         public DbSet<Coach> Coaches { get; set; }
     }
---
+++ EntityFrameworkCore.Data/Migrations/20250218091259_SeededTeams.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EntityFrameworkCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededTeams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "TeamId", "CreatedDate", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2023, 1, 1, 1, 1, 1, 0, DateTimeKind.Utc), "Liverpool" },
+                    { 2, new DateTime(2023, 1, 1, 2, 2, 2, 0, DateTimeKind.Utc), "Arsenal" },
+                    { 3, new DateTime(2023, 1, 1, 3, 3, 3, 0, DateTimeKind.Utc), "Tottenham Hotspur" },
+                    { 4, new DateTime(2023, 1, 1, 4, 4, 4, 0, DateTimeKind.Utc), "Manchester City" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "TeamId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "TeamId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "TeamId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "TeamId",
+                keyValue: 4);
+        }
+    }
+}
