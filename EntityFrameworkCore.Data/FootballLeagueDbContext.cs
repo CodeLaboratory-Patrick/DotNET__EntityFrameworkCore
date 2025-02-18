42c707c2a1f100b8974dea663c1c958e1dde7170
Donghwi_Park <gptnc5100@gmail.com>
2025-02-18 08:39:29 +0100

    Adding a Migration

--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -13,7 +13,7 @@ namespace EntityFrameworkCore.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Using SQL Server
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeage_EfCore; Encrypt=False");
+            //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeage_EfCore; Encrypt=False");
 
             //Using SQLite
             optionsBuilder.UseSqlite($"Data Source=FootballLeage_EfCore.db");
