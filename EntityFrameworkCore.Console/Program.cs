c328f0cb625a7c0d981aa5023ec55f4f05e81d2c
Donghwi_Park <gptnc5100@gmail.com>
2025-02-19 09:00:48 +0100

    Querying for a Single Record

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,14 +1,33 @@
 ﻿using EntityFrameworkCore.Data;
+using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
 
 //First we need an instance of Context
-var context = new FootballLeagueDbContext();
+using var context = new FootballLeagueDbContext();
 
 //Select all the teams in a database
-//SELECT * FROM Teams
+//GetAllTeams();
 
-var teams = context.Teams.ToList();
+//Selecting a single record - First one in the list
+var teamOne = await context.Teams.FirstAsync();
 
-foreach (var team in teams)
+//Selecting a single record - First one in the list that meets a condition
+var teamTwo = await context.Teams.FirstAsync(team => team.TeamId == 1);
+//var teamTwo = await context.Teams.FirstOrDefaultAsync(team => team.TeamId == 1);
+
+//Selecting a single record - Only we record should be returned
+var teamThree = await context.Teams.SingleAsync(team => team.TeamId == 2);
+//var teamThree = await context.Teams.SingleOrDefaultAsync(team => team.TeamId == 2);
+
+//Selecting based on ID
+var TeamBasedOnId = await context.Teams.FindAsync();
+
+void GetAllTeams()
 {
-    Console.WriteLine($"TeamId: {team.TeamId}, Name: {team.Name}, CreatedDate: {team.CreatedDate}");
+    //SELECT * FROM Teams
+    var teams = context.Teams.ToList();
+    foreach (var t in teams)
+    {
+        Console.WriteLine($"TeamId: {t.TeamId}, Name: {t.Name}, CreatedDate: {t.CreatedDate}");
+    }
 }
